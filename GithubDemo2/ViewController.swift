cad75eebc74dd427a4ad738d0de03c491bc5f351
Khasan <ixanaxy@gmail.com>
2022-02-15 17:49:46 +0500

    Initial Commit

---
+++ GithubDemo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubDemo2
+//
+//  Created by Xasan Xasanov on 15/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
